1842160d4786c9335d28c6cba064e64f9118850a
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-04-25 12:40:47 -0300

    Exercicio AnaliseCredito

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercicio_AnaliseCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              decimal emprestimo, renda, parcelas , calcporcentagem, calcparcela;
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Quantia do Empréstimo...: ");
+            emprestimo = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Parcelas...: ");
+            parcelas = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Sua Renda Mensal...: ");
+            renda = decimal.Parse(Console.ReadLine());
+            Console.ResetColor();
+
+
+                    calcparcela = parcelas / emprestimo;
+                    calcporcentagem = (30 * renda) / 100;
+
+
+
+                
+                 if (calcporcentagem < calcparcela) 
+                {
+                     Console.ForegroundColor = ConsoleColor.Red;
+                     Console.WriteLine("Não foi possivel o seu emprestimo.");
+                     Console.ResetColor();
+                } 
+                 
+            
+
+                
+                 else 
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Emprestimo bem sucedido!!!");
+                    Console.ResetColor();
+                }
+            
+        }
+    }
+}
